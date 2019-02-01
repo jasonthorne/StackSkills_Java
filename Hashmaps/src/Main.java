adf6757f1fd7c5e89ed6c1d2e9b90f8e185f5ac8
JasonThorne <jasonthorne@live.ie>
2019-02-01 00:28:32 +0000

    3. Data Structures and collections (Hashmaps)

---
+++ Hashmaps/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashMap users = new HashMap(); //+++IMPORTANT: hashmaps have key and value pairs.
+
+        users.put("Bob", 34); //"Bob" is the key. "34" is the value
+        users.put("Jeff", 64);
+
+        if (users.containsKey("Jeff")){
+            System.out.println("Jeff is here!");
+        }
+
+        System.out.println("Bobs value is: " + users.get("Bob"));
+
+        System.out.println("All values: " + users.values());
+        System.out.println("All keys and values:" + users.entrySet());
+
+        //loop through Hashmap
+        Iterator it = users.entrySet().iterator();
+
+        while(it.hasNext()){ //while 'it' is pointing to another value in hashmap
+            Map.Entry pair = (Map.Entry) it.next(); //cast 'it.next' to be of type 'map entry'. Map is a higher level collections class.
+            //'pair' of type 'Map.entry' now has everything thats inside of 'users' hashmap.
+
+            System.out.println(pair.getKey() + " = " + pair.getValue());
+        }
+
+
+
+
+
+
+    }
+}
