41b46747251f0031f7b8151ab13636df0c2ab30d
JasonThorne <jasonthorne@live.ie>
2019-02-04 23:34:15 +0000

    7. Data Structures and collections. Static types

--- StaticTypes/src/Main.java
+++ StaticTypes/src/Main.java
@@ -2,6 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        int myVal = -45;
+        int myVal2 = 56;
+
+       //System.out.println(Math.abs(myVal));
+
+        //System.out.println(Math.min(myVal, myVal2));
+
+        int x = Math.min(myVal, myVal2);
+        int y = Math.abs(myVal);
+
+        System.out.println("x " + x + " y " + y);
+
+        //Class methods should be static if instant variables arent required within the methods of those classes
+
     }
 }
