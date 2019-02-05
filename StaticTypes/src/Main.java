e9af7814a42765c15ec6ab1a56a7717aba8aaac2
JasonThorne <jasonthorne@live.ie>
2019-02-05 19:10:53 +0000

    9. Data Structures and collections. Static types

--- StaticTypes/src/Main.java
+++ StaticTypes/src/Main.java
@@ -4,28 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int myVal = -45;
-        int myVal2 = 56;
-
-        //System.out.println(Main.set);
-
-        //StaticProj myProj = new StaticProj();
-        //System.out.println(setName());
-
-       //System.out.println(Math.abs(myVal));
-
-        //System.out.println(Math.min(myVal, myVal2));
-
-        //int x = Math.min(myVal, myVal2);
-       // int y = Math.abs(myVal);
-
-       // System.out.println("x " + x + " y " + y);
-
-        //Class methods should be static if instant variables arent required within the methods of those classes
+        name = "Dawg!";
+        Abc.show(); //no object of class needed to invoke method as method is static
+        System.out.println(name);
+        System.out.println(Abc.i); //no object of class needed to see var as var is static
 
     }
 
-    public static void setName(String n) {
-        name = n;
+}
+
+class Abc
+{
+    static int i=5;
+
+    public static void show(){
+        System.out.println("Yo");
+        //System.out.println(i);
     }
+
 }
