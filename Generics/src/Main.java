4309be93a80e2802712d08e4e14837e6def0d40a
JasonThorne <jasonthorne@live.ie>
2019-02-06 22:26:17 +0000

    7. OOP Part 3 - Generics

--- Generics/src/Main.java
+++ Generics/src/Main.java
@@ -1,4 +1,17 @@
 
+class Person {
+    private String name;
+    private String password;
+
+    //getters & setters
+    public String getPersonName() { return name; }
+    public void setPersonName(String name) { this.name = name; }
+    public String getPersonPassword() { return password; }
+    public void setPersonPassword(String password) { this.password = password; }
+}
+
+
+
 //Generic class with template which accepts generic type 'T' (I.E. an object of any type)
 //Angular brackets are used for generics instead of brackets
 class Login<T>{
@@ -24,10 +37,21 @@ public class Main {
         //System.out.println("Hello World!");
 
         //Angular brackets are used for generics instead of brackets
-        Login<String> login = new Login<String>("Paul", "password");
-        //Login<Integer> login = new Login<Integer>(5, 7); //another example of a login object
+        Login<String> login1 = new Login<String>("Paul", "password");
+        Login<Integer> login2 = new Login<Integer>(5, 7); //another example of a login object
 
 
-        System.out.println(login.getName());
+        System.out.println(login1.getName() + " " + login2.getName());
+
+        //---------
+        Person person = new Person();
+        person.setPersonName("Bob");
+        person.setPersonPassword("123");
+
+        Login<Person> login3 = new Login<Person>(person, person);
+
+        System.out.println(login3.getName().getPersonName());
+        //-----------
+
     }
 }
