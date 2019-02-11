ff2aa136e4b1e2b7abb80e87f098550046398c2a
JasonThorne <jasonthorne@live.ie>
2019-02-11 00:59:29 +0000

    2. Intro to Swing (AWT) - SwingDemo 1 - JFrame

--- IntroToSwing/src/com/jay/SwingDemo/SwingDemo.java
+++ IntroToSwing/src/com/jay/SwingDemo/SwingDemo.java
@@ -7,7 +7,7 @@ public class SwingDemo {
     //class constructor (to be called in main)
     public SwingDemo(){
 
-        //create new JFrame container object. Frame is part of a 'container class'
+        //create new JFrame container object, and give it a title. Frame is part of a 'container class'
         JFrame jFrame = new JFrame("First Swing Application"); //passing in the title of the frame as string
 
         //Give the frame as size
@@ -16,10 +16,25 @@ public class SwingDemo {
         //Terminate the program when the user closes the application
         jFrame.setDefaultCloseOperation(jFrame.EXIT_ON_CLOSE);
 
+        //Show the frame
+        jFrame.setVisible(true);
+
     }
 
     public static void main (String[] args){
         System.out.println("Yo");
+
+        //create the frame on the event dispatching thread
+        //'.invokeLater' ensures that there's no conflict between this and other running processes VERY IMPORTANT!!!
+        SwingUtilities.invokeLater(new Runnable() { //enstatiating a new 'Runnable' class which is anonymous. IE runs immediately, no need to create, extantiate and THEN run it.
+            @Override
+            public void run() {
+
+                new SwingDemo(); //instantiating swing demo class and directly calling its constructor
+            }
+        });
+
+
 
 
 
