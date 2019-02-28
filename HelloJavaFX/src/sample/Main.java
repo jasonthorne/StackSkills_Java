48194cb2886f59c4e859686f974ed98928ae5510
jasonthorne <jasonthorne@live.ie>
2019-02-28 23:04:08 +0000

    1. JavaFX

---
+++ HelloJavaFX/src/sample/Main.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application { //'Application' class allows for the creation of an FX application
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args); //this launches the app!
+    }
+}
