aaa59f32e35b859d669de723041530c0a3fc4ef3
jasonthorne <jasonthorne@live.ie>
2019-03-02 12:36:31 +0000

    7. JavaFX - App Fruits, part 1. 2

--- CheckBoxes/src/sample/Main.java
+++ CheckBoxes/src/sample/Main.java
@@ -2,8 +2,10 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Orientation;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,8 +16,17 @@ public class Main extends Application {
         //give stage a title:
         primaryStage.setTitle("my title");
 
+        //create a root (flowpane in this case):
+        FlowPane flowPaneRoot = new FlowPane(Orientation.VERTICAL, 5, 5); //orientation (if you want its elements to be vertically or horizontally oriented), horiz gap, vert gap.
 
+        //create scene (with a height and width - IMPORTANT!!) and give it the root:
+        Scene scene = new Scene(flowPaneRoot, 400, 250);
 
+        //add scene to stage:
+        primaryStage.setScene(scene);
+
+        //show the stage (and its child elements)
+        primaryStage.show();
 
     }
 
