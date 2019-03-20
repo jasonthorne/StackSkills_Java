6f4beeeeb1d9284f887feef5de7b12231f534e6a
JasonThorne <jasonthorne@live.ie>
2019-03-20 17:26:41 +0000

    Lambda Expressions. Expressions with multiple Parameters

--- IntroToLambdas/src/Main.java
+++ IntroToLambdas/src/Main.java
@@ -2,10 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Regular way of instantiating a drawable obj:
+        //+++++++++++++++++++++++++++++++++++++++++++++++++++1
+        /*
+        //Old way of instantiating a drawable obj:
         Drawable drawable1 = new Drawable() {
             @Override
-            public void draw() {
+            public void draw(int width, int height) { //Normally method needs declared and overriden++++++++
                 System.out.println("Drawing...");
             }
 
@@ -13,17 +15,34 @@ public class Main {
 
 
         //invoke drawable method:
-        drawable1.draw();
+        drawable1.draw(33, 0);
+*       */
+        //+++++++++++++++++++++++++++++++++++++++++++2=
 
+        /*
+        //==============================================
+        //Lambda way of instantiating a drawable obj:
+        Drawable drawable2 = (width, height) ->{ //brackets for input params as usual
+            System.out.println("Lambda Drawing..." + width + " " +  height);
 
-        //Lambda way:
-        Drawable drawable2 = () ->{
-            System.out.println("Lambda Drawing...");
-
-        };
+        };//Dont forget this colon!! +++
 
         //invoke Lambda drawable method:
-        drawable2.draw();
+        drawable2.draw(23, 5);
+
+
+        //=============================================
+        */
+        
+        Addable addable = (a,b)->(a+b); //passing a & b as params, declaring its method operation to be a + b
+        //invoke addable method:
+        System.out.println(addable.add(1, 3));
+
+
+        //Can also SPECIFY what input type to pass as params ('int a'):
+        Addable addable2 = (int a, int b)->(a+b); //passing a & b as params, declaring its method operation to be a + b
+        //invoke addable method:
+        System.out.println(addable2.add(5, 3));
 
     }
 
@@ -35,7 +54,13 @@ public class Main {
 //create an interface class:
 interface Drawable{
 
-    public void draw(); //no body as its an interface (made for implementation)
+    //From commented out 'Old way of instantiating a drawable obj':
+    //public void draw(int width, int height); //no body as its an interface (made for implementation)
+
+    public void draw(int width, int height);
+}
 
 
+interface  Addable{
+   public int add(int a, int b); //method takes in int an and b, and returns an int (as not 'void')
 }
